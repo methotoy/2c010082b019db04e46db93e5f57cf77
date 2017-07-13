65c5a5276392ff1cc28103f1f2184cc5598c591e
Methotoy <emeritobucogii@gmail.com>
2017-07-13 18:29:23 +0800

    Contact Us Page and start of Term of use Page.

--- src/pages/terms-of-use/terms-of-use.ts
+++ src/pages/terms-of-use/terms-of-use.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+import { AppServiceProvider } from './../../providers/app-service/app-service';
 
 /**
  * Generated class for the TermsOfUsePage page.
@@ -11,14 +14,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-terms-of-use',
   templateUrl: 'terms-of-use.html',
+  animations: [
+    trigger('fade', [
+      state('visible', style({
+        opacity: 1
+      })),
+      state('invisible', style([{
+        opacity: 0, display: 'none'
+      }])),
+      transition('* => *', animate('1s'))
+    ])
+  ]
 })
-export class TermsOfUsePage {
+export class TermsOfUsePage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  public termsOfUseData = null;
+
+  constructor(
+    private navCtrl: NavController,
+    private navParams: NavParams,
+    private appService: AppServiceProvider
+  ) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TermsOfUsePage');
+  }
+
+  ngOnInit() {
+    this.appService.getCms('terms-of-use')
+      .subscribe(
+      (response) => {
+        let responseData = (response && response.length > 0) ? response[0] : response;
+        if (typeof (responseData) && 'cmsContent' in responseData) {
+          this.termsOfUseData = responseData.cmsContent.split('..').join('https://pizzacrust.com.pk');
+          this.termsOfUseData = this.termsOfUseData.split('<p>&nbsp;</p>').join('');
+          this.termsOfUseData = this.termsOfUseData.split('Website').join('Mobile Application');
+          this.termsOfUseData = this.termsOfUseData.split('Web site').join('Mobile Application');
+          this.termsOfUseData = this.termsOfUseData.split('by clicking on the “Terms of Use” located at the bottom of the pages').join("on 'Terms of Use' page");
+          this.termsOfUseData = this.termsOfUseData.split('AND/OR VISITING THIS WEBSITE').join("THIS MOBILE APPLICATION");
+          this.termsOfUseData = this.termsOfUseData.split('Pizzacrust.com.pk domain name').join("application");
+          this.termsOfUseData = this.termsOfUseData.split('computer').join("device");
+          this.termsOfUseData = this.termsOfUseData.split('web site').join("application");
+          this.termsOfUseData = this.termsOfUseData.split('website').join("application");
+        }
+      },
+      (error) => console.error(error)
+      );
   }
 
 }
